86aeeb6605323c3d1605c9300bc5cb26b7eae389
Anton <antonmakovei32@gmail.com>
2023-09-07 20:24:42 +0300

    Firts commit

--- react-new-one/src/App.js
+++ react-new-one/src/App.js
@@ -4,20 +4,42 @@ import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:3001/tasks').then(res => {
-      console.log(res)
-      setTasks(res.data)
-    })
+    getTasks()
   }, [])
+
+  const getTasks = () => {
+    axios.get('http://127.0.0.1:3001/tasks').then(res => {
+      setTasks(res.data)
+    }).catch(e => console.log('^.^ - error:' , e.toString()))
+  }
+
+  const handleTaskCreation = () => {
+    axios.post('http://127.0.0.1:3001/tasks', {name: newTask}).then(r => {
+      if(r.data.isCreated) {
+        setNewTask('')
+        getTasks()
+      }
+    })
+  }
 
   return (
     <div className="App">
-      {tasks.map((id, name) => <ul key={id}>
-        <li>id: {id}</li>
-        <li>name: {name}</li>
-      </ul>)}
+      <div className="wrapper">
+        {tasks.map(({id, name}) => <ul className="list" key={id}>
+          <li>id: <span><strong>{id}</strong></span></li>
+          <li>name: <span>{name}</span></li>
+          <li className="checkBoxItem">completed: <input type="checkbox"/> </li>
+        </ul>)}
+      </div>
+      <div className="inputArea">
+        <input type="text" value={newTask} onChange={e => {
+          setNewTask(e.target.value)
+        }} />
+        <button onClick={() => handleTaskCreation()}>Create new Task</button>
+      </div>
     </div>
   );
 }
